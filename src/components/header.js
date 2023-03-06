5a82e8f86a7f4fb4bd1cb59a3fb8fad508605086
madyanes <madyanes.1995@gmail.com>
2023-03-07 05:12:39 +0700

    styling heading

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,17 @@
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
 export default function Header() {
   return (
-    <header>
-      <h1>ian's blog</h1>
-    </header>
+    <>
+      <div className="grid place-items-center">
+        <div className="text-5xl font-bold self-center mx-auto bg-gradient-to-r from-green-600 to-blue-500 bg-clip-text text-transparent py-2">
+          ian's weblog
+        </div>
+        <div className="italic font-semibold px-2 py-1 text-xs bg-yellow-400">
+          I know it is impossible, until I make it.
+        </div>
+      </div>
+    </>
   )
 }
