23e04e445e97cd232a3dfde9c8b3f5c9d486fe7b
madyanes <madyanes.1995@gmail.com>
2023-03-08 17:26:37 +0700

    restyling document header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Header() {
   return (
     <>
-      <div className="grid place-items-center">
+      <div className="grid place-items-center py-2 md:py-10">
         <div className="text-5xl font-bold self-center mx-auto bg-gradient-to-r from-green-600 to-blue-500 bg-clip-text text-transparent py-2">
           ian's weblog
         </div>
