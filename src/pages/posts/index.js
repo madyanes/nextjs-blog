8c7798e698caa689a477dca53704311053845837
madyanes <madyanes.1995@gmail.com>
2023-03-10 15:17:42 +0700

    applying query param for next page

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export async function getServerSideProps() {
   const apiURL = `${ process.env.NEXT_PUBLIC_API_BACKEND }/api/posts/`
@@ -14,16 +15,30 @@ export async function getServerSideProps() {
 export default function Posts(props) {
   const { postsData } = props
   const posts = postsData.results
+  const nextPage = postsData.next
+  const router = useRouter()
+
+  let nextPageLink
+  if (router.query.page) {
+    nextPageLink = router.pathname + '?page=' + (parseInt(router.query.page) + 1)
+  } else {
+    nextPageLink = router.pathname + '?page=2'
+  }
+
   return (
     <>
       <Link href='/posts/new' className='bg-green-500 text-sm shadow-lg border-black border-2 font-extrabold rounded-full text-center px-2 fixed bottom-5 left-1/2 -translate-x-1/2'>+ New Post</Link>
       { posts.map(post => 
         <div className="pb-3" key={ post.id }>
+          <p className='font-semibold text-lg'>
+            <Link href={'#' + post.id}>{ post.title }</Link>
+          </p>
           <p>
-            <strong className="font-semibold">{ post.id }. { post.title }</strong> { post.content }
+            { post.content }
           </p>
         </div>
       ) }
+      { nextPage && <Link href={ nextPageLink }>Next Page</Link> }
     </>
   )
 }
