1f34dc12b1a78a9cc356693a17fbf5ef733099c3
madyanes <madyanes.1995@gmail.com>
2023-03-12 10:52:11 +0700

    restyling header; centering text

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ export default function Header() {
   return (
     <>
       <div className="bg-green-200">
-        <div className={`${ headerAnimation } text-6xl font-bold bg-clip-text text-transparent py-2`}>
-          ian&apos;s weblog
+        <div className={`${ headerAnimation } text-center text-5xl font-bold bg-clip-text text-transparent py-2`}>
+          &lt;ian&apos;s weblog /&gt;
         </div>
-        <div className={`${ headerAnimation } italic font-semibold px-2 py-1 text-xs`}>
+        <div className={`${ headerAnimation } text-center italic font-semibold px-2 py-1 text-xs`}>
           <span className='text-white font-black'>I know it is impossible, until I make it.</span>
         </div>
       </div>
