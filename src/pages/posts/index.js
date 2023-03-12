066a2e9ae24a3f390ace4f1853385d15dd8c1813
madyanes <madyanes.1995@gmail.com>
2023-03-12 21:57:49 +0700

    bugfix; new post goes behind posts

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -32,7 +32,7 @@ export default function Posts(props) {
 
   return (
     <>
-      <Link href='/posts/new' className='bg-green-500 text-sm shadow-lg border-black border-2 font-extrabold rounded-full text-center px-2 fixed bottom-5 left-1/2 -translate-x-1/2'>+ New Post</Link>
+      <Link href='/posts/new' className='z-10 bg-green-500 text-sm shadow-lg border-black border-2 font-extrabold rounded-full text-center px-2 fixed bottom-5 left-1/2 -translate-x-1/2'>+ New Post</Link>
       { posts.map(post => 
         <div className="bg-white drop-shadow-md hover:drop-shadow-xl dark:bg-black px-4 py-5 mb-1 rounded-lg" key={ post.id }>
           <p className="text-xs">{ post.created_at }</p>
