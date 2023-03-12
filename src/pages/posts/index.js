47f4a61ff5c78dd962f3bd0f4737a57e1f8215af
madyanes <madyanes.1995@gmail.com>
2023-03-12 16:18:05 +0700

    display created date

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -35,6 +35,8 @@ export default function Posts(props) {
       <Link href='/posts/new' className='bg-green-500 text-sm shadow-lg border-black border-2 font-extrabold rounded-full text-center px-2 fixed bottom-5 left-1/2 -translate-x-1/2'>+ New Post</Link>
       { posts.map(post => 
         <div className="bg-white dark:bg-black px-4 py-5 mb-1 rounded-lg border-slate-200 border-2" key={ post.id }>
+          <p className="text-xs">{ post.created_at }</p>
+          <hr className="my-1" />
           <p className='font-semibold text-lg'>
             <Link href={ `${ router.pathname }/${ post.id }` }>{ post.title }</Link>
           </p>
