25e9806161185cfe008f4f75fd473b06bcfd09f5
madyanes <madyanes.1995@gmail.com>
2023-03-15 17:05:28 +0700

    fix: update DOM after post deletion

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Pagination from '@/components/pagination'
 import callAPI from '@/helpers/callAPI'
@@ -27,13 +28,19 @@ export async function getServerSideProps(context) {
 }
 
 export default function Posts(props) {
+  const [postList, setPostList] = useState([])
   const { postsData } = props
-  const posts = postsData.results
   const router = useRouter()
+
+  useEffect(() => {
+    setPostList(postsData.results)
+  }, [postsData.results])
 
   const deletePost = (postId) => {
     try {
       callAPI('DELETE', `${process.env.NEXT_PUBLIC_API_BACKEND}/api${router.pathname }/${ postId }/`)
+      const updatedPostList = postList.filter(post => post.id !== postId)
+      setPostList(updatedPostList)
     } catch(err) {
       console.error(err)
     }
@@ -42,7 +49,7 @@ export default function Posts(props) {
   return (
     <>
       <Link href='/posts/new' className='z-10 bg-green-500 text-sm shadow-lg border-black border-2 font-extrabold rounded-full text-center px-2 fixed bottom-5 left-1/2 -translate-x-1/2'>+ New Post</Link>
-      { posts.map(post => 
+      { postList.map(post => 
         <div className="bg-white drop-shadow-md hover:drop-shadow-xl dark:bg-black px-4 py-5 mb-1 rounded-lg" key={ post.id }>
           <p className="text-xs">{ post.created_at } | <button onClick={ () => deletePost(post.id) }>DELETE</button></p>
           <hr className="my-1" />
